c778bb2f09ade59c0510ce690a38331632468e08
Dilyana Dimitrova <dilyanadimitrova55@gmail.com>
2024-08-02 19:32:30 +0900

    js, CSS, ejs files update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
+const api_key = "e9c5a4a0d893f4a09aed99d3541cd56c16bfca4a";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,10 +16,29 @@ app.get("/", (req, res) => {
 app.post("/", async(req, res) => {
     
     try {
-        const response = await axios.get(`https://restcountries.com/v3.1/name/${req.body.country}`);
-        // console.log(response.data[0].name.common);
-        // console.log(response.data[0].capital[0]);
-        res.render("index.ejs", {data: response.data[0]})
+        const response_country = await axios.get(`https://restcountries.com/v3.1/name/${req.body.country}`);
+        // console.log(response_country.data[0].name.common);
+        // console.log(response_country.data[0].capital[0]);
+        const country_code = response_country.data[0].cca2;
+        //console.log(country_code);
+        
+
+        if(req.body.year){
+            const response_calendar = await axios.get(
+                `https://calendarific.com/api/v2/holidays?` +
+                `&api_key=${api_key}` +
+                `&country=${country_code}` +
+                `&year=${req.body.year}` +
+                `&month=${req.body.month}`
+              );
+              // console.log(response_calendar.data.response.holidays[0]);
+              res.render("index.ejs", {
+                country: response_country.data[0],
+                calendar: response_calendar.data.response.holidays
+              });
+        } else {
+            res.render("index.ejs", {data: response_country.data[0]});
+        }        
     } catch (error) {
         console.log(error.message);
     }
